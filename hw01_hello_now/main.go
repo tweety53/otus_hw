12e318a8936bbc6f86f6d0a768a6117490adabfa
Yuri Alexandrov <yatweety@gmail.com>
2020-09-10 22:15:36 +0300

    HW1 is completed (#1)
    * HW1 is completed
    
    * fix go.mod module path
    
    * upd to 24h layout
    
    Co-authored-by: yuriy aleksandrov <yuriy.aleksandrov@iqoption.com>

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	const timeLayout = "2006-01-02 15:04:05 -0700 MST"
+
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("ntp.Time() error: %s", err)
+	}
+
+	fmt.Printf("current time: %s\n", time.Now().Format(timeLayout))
+	fmt.Printf("exact time: %s\n", ntpTime.Format(timeLayout))
 }
