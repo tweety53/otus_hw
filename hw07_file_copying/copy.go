9f983de08b6f6e83dc32933fb762107a6109d06a
yuriy aleksandrov <yuriy.aleksandrov@iqoption.com>
2020-12-29 19:14:35 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -9,7 +14,67 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+func Copy(fromPath, toPath string, offset, limit int64) error {
+	//fromPath validation
+	fi, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if !fi.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	//offset validation
+	if offset > fi.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	limit = adjustLimit(limit, fi.Size(), offset)
+
+	var srcFile *os.File
+
+	srcFile, err = os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err = srcFile.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	dstFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err = dstFile.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	_, err = srcFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(srcFile)
+
+	_, err = io.CopyN(dstFile, barReader, limit)
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	bar.Finish()
+
 	return nil
+}
+
+func adjustLimit(limit int64, fileSize int64, offset int64) int64 {
+	if limit == 0 || limit > fileSize-offset {
+		limit = fileSize - offset
+	}
+
+	return limit
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,121 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const OutFileName = "out.txt"
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	defer os.Remove(OutFileName)
+
+	t.Run("src file not exists err", func(t *testing.T) {
+		err := Copy("random_file_name.txt", OutFileName, 0, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("src file is not regular", func(t *testing.T) {
+		err := Copy("/dev/urandom", OutFileName, 0, 0)
+		require.EqualError(t, err, ErrUnsupportedFile.Error())
+	})
+
+	t.Run("offset exceeds src file size", func(t *testing.T) {
+		tmpFile, err := ioutil.TempFile("", "copy_test")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(tmpFile.Name())
+
+		_, err = tmpFile.WriteString("qwer")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = Copy(tmpFile.Name(), OutFileName, 5, 0)
+		require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
+	})
+
+	srcFileData := []byte{'q', 'w', 'e'}
+	successTestCases := []struct {
+		limit               int64
+		offset              int64
+		srcFileData         []byte
+		expectedOutFileData []byte
+	}{
+		{0, 0, srcFileData, []byte{'q', 'w', 'e'}},
+		{1000, 0, srcFileData, []byte{'q', 'w', 'e'}},
+		{1, 0, srcFileData, []byte{'q'}},
+		{2, 0, srcFileData, []byte{'q', 'w'}},
+		{0, 1, srcFileData, []byte{'w', 'e'}},
+		{0, 1, srcFileData, []byte{'w', 'e'}},
+		{0, 2, srcFileData, []byte{'e'}},
+		{0, 3, srcFileData, []byte{}},
+		{1, 1, srcFileData, []byte{'w'}},
+		{2, 2, srcFileData, []byte{'e'}},
+	}
+
+	for _, tt := range successTestCases {
+		t.Run(fmt.Sprintf("success Copy with limit=%d,offset=%d", tt.limit, tt.offset), func(t *testing.T) {
+			tmpFile, err := ioutil.TempFile("", "copy_test")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer os.Remove(tmpFile.Name())
+
+			_, err = tmpFile.Write(tt.srcFileData)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = Copy(tmpFile.Name(), OutFileName, tt.offset, tt.limit)
+			require.NoError(t, err)
+
+			outFile, err := os.Open(OutFileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer func() {
+				if err = outFile.Close(); err != nil {
+					log.Fatal(err)
+				}
+			}()
+
+			outFileData, err := ioutil.ReadFile(OutFileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			require.Equal(t, tt.expectedOutFileData, outFileData)
+		})
+	}
+}
+
+func TestAdjustLimit(t *testing.T) {
+
+	tests := []struct {
+		name        string
+		limitArg    int64
+		fileSize    int64
+		offset      int64
+		expectedRes int64
+	}{
+		{"limit not defined or zero case", 0, 666, 0, 666},
+		{"limit exceeds bytes to write size ", 667, 666, 0, 666},
+		{"limit exceeds bytes to write size, with offset", 567, 666, 100, 566},
+		{"ok case", 3, 666, 0, 3},
+		{"ok case, with offset", 3, 666, 333, 3},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := adjustLimit(tt.limitArg, tt.fileSize, tt.offset)
+			require.Equal(t, tt.expectedRes, res)
+		})
+	}
 }
